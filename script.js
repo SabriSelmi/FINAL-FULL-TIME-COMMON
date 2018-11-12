fc00fd9130d4f4fef78f41ec5f050e6b92aa37af
unknown <sabri.selmi1992@gmail.com>
2018-11-12 12:15:31 +0100

    4th commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayNone(){
+    document.getElementById('tour').style.display='none';
+}
+function display(){
+    if ( document.getElementById('tour').style.display=='none')
+    document.getElementById('tour').style.display='block';
+    else
+    document.getElementById('tour').style.display='none';
+}
